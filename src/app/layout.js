016011efd2554836e35ca3383d1085cc9788abd8
cristito <cdpsk8@gmail.com>
2024-05-24 18:27:36 -0500

    cambio nav

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
+      <body className={inter.className}>      
         {children}
+        <Navbar />
       </body>
     </html>
   )
